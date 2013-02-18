d1ddec086f666d7f82f340c25a5d748140d46cd1
natewalck <nate@osxadm.in>
2013-02-18 18:41:54 -0500

    Added more apps to my applications manifest

--- natewalck/applications.pp
+++ natewalck/applications.pp
@@ -1,3 +1,11 @@
 class people::natewalck::applications {
   include minecraft
+  include chrome
+  include spotify
+  include onepassword
+  include macvim
+  include iterm2::stable
+  include dropbox
+  include gitx
+  include flux
 }
