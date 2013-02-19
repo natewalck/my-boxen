fb97b43772203c7762c6f978f1b1aff7338cc1b8
natewalck <nate@osxadm.in>
2013-02-18 21:16:30 -0500

    Added dotfiles settings and vim settings

--- natewalck.pp
+++ natewalck.pp
@@ -1,6 +1,7 @@
 class people::natewalck {
   include people::natewalck::applications
   include people::natewalck::plist-settings
+  include people::natewalck::dotfile-settings
   # Notfiy is for debugging purposes
   #notify { 'This is Nates stuff, mind your bidness': }
 }
---
+++ natewalck/dotfile-settings.pp
@@ -0,0 +1,21 @@
+class people::natewalck::dotfile-settings {
+
+  $my_homedir = "/Users/${::luser}"
+
+  exec { "Clone vim Settings":  
+    command => "git clone --recursive https://github.com/natewalck/vim.git ${my_homedir}/.vim",
+    creates => "${my_homedir}/.vim",
+  }
+
+  file { 'vim Settings':
+    ensure  => directory,
+    require => Exec['Clone vim Settings'],
+    path => "${my_homedir}/.vim",
+    mode => '600',
+  }
+
+  file { "${my_homedir}/.vimrc":
+    ensure => link,
+    target => "${my_homedir}/.vim/vimrc",
+  }
+}
