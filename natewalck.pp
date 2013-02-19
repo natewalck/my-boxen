8d2bed1061233307617579a83f48fa2c284adfda
natewalck <nate@osxadm.in>
2013-02-18 19:43:28 -0500

    included new plist settings class

--- natewalck.pp
+++ natewalck.pp
@@ -1,5 +1,6 @@
 class people::natewalck {
   include people::natewalck::applications
+  include people::natewalck::plist-settings
   # Notfiy is for debugging purposes
   #notify { 'This is Nates stuff, mind your bidness': }
 }
