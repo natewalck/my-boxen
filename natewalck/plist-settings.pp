dda2d2a841501d1f9e54eba6bc465819b42d3f23
natewalck <nate@osxadm.in>
2013-02-18 19:42:34 -0500

    Added a plist settings class

---
+++ natewalck/plist-settings.pp
@@ -0,0 +1,16 @@
+class people::natewalck::plist-settings {
+
+  property_list_key { 'Lower Right Hotcorner - Screen Saver':
+    ensure     => present,
+    path       => "${my_homedir}/Library/Preferences/com.apple.dock.plist",
+    key        => 'wvous-br-corner',
+    value      => 10,
+    value_type => 'integer',
+    notify     => Exec['Restart the Dock'],
+  }
+
+  exec { 'Restart the Dock':
+    command     => '/usr/bin/killall -HUP Dock',
+    refreshonly => true,
+  }
+}
