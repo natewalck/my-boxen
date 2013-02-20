69f25cc7e4a8bd68ef97fc42e0682bb644a3d82f
natewalck <nate@osxadm.in>
2013-02-19 21:22:43 -0500

    Added GlobalPreferences class

--- natewalck.pp
+++ natewalck.pp
@@ -2,6 +2,7 @@ class people::natewalck {
   include people::natewalck::applications
   include people::natewalck::settings::dock
   include people::natewalck::settings::finder
+  include people::natewalck::settings::globalprefs
   include people::natewalck::settings::dotfiles
   # Notfiy is for debugging purposes
   #notify { 'This is Nates stuff, mind your bidness': }
---
+++ natewalck/settings/globalprefs.pp
@@ -0,0 +1,23 @@
+class people::natewalck::settings::globalprefs {
+
+  $my_homedir = "/Users/${::luser}"
+
+  property_list_key { 'Double Click Minimize':
+    ensure     => present,
+    path       => "${my_homedir}/Library/Preferences/.GlobalPreferences.plist",
+    key        => 'AppleMiniaturizeOnDoubleClick',
+    value      => true,
+    value_type => 'boolean',
+    notify     => Exec['Restart Finder'],
+  }
+
+  file { 'GlobalPrefs Plist':
+    ensure  => file,
+    require => [
+                 Property_list_key['Double Click Minimize'],
+               ],
+    path    => "${my_homedir}/Library/Preferences/.GlobalPreferences.plist",
+    mode    => '0600',
+    notify     => Exec['Restart Finder'],
+  }
+}
