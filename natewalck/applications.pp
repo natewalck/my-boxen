4505bffe82f91f7759bcfd7faeacc9a817ff8b97
natewalck <nate@osxadm.in>
2013-02-23 17:52:19 -0500

    Added adium and changed iTerm to dev branch

--- natewalck/applications.pp
+++ natewalck/applications.pp
@@ -4,7 +4,7 @@ class people::natewalck::applications {
   include spotify
   include onepassword
   #include macvim
-  include iterm2::stable
+  include iterm2::dev
   include dropbox
   include gitx
   include flux
@@ -17,5 +17,10 @@ class people::natewalck::applications {
   package { 'Crashplan':
     source           => 'http://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_3.4.1_Mac.dmg',
     provider => pkgdmg,
+  }
+
+  package { 'Adium':
+    source   => 'http://downloads.sourceforge.net/project/adium/Adium_1.5.4.dmg',
+    provider => appdmg,
   }
 }
