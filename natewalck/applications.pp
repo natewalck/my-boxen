53e5c10b572b72da3156c4d07db9f9b200ede237
natewalck <nate@osxadm.in>
2013-02-23 18:18:08 -0500

    Added a bunch of applications

--- natewalck/applications.pp
+++ natewalck/applications.pp
@@ -23,4 +23,39 @@ class people::natewalck::applications {
     source   => 'http://downloads.sourceforge.net/project/adium/Adium_1.5.4.dmg',
     provider => appdmg,
   }
+
+  package { 'AppCleaner':
+    source   => 'http://www.freemacsoft.net/downloads/AppCleaner_2.1.zip',
+    provider => compressed_app,
+  }
+
+  package { 'CyberDuck':
+    source   => 'http://cyberduck.ch/Cyberduck-4.2.1.zip',
+    provider => compressed_app,
+  }
+
+  package { 'GrandPerspective':
+    source   => 'http://sourceforge.net/projects/grandperspectiv/files/grandperspective/1.5.1/GrandPerspective-1_5_1.dmg',
+    provider => appdmg,
+  }
+
+  package { 'Keka':
+    source   => 'http://www.kekaosx.com/release/Keka-1.0.3-intel.dmg',
+    provider => appdmg,
+  }
+
+  package { 'owncloud':
+    source   => 'http://download.owncloud.com/download/ownCloud-1.2.0.dmg',
+    provider => appdmg,
+  }
+
+  package { 'Skype':
+    source   => 'http://download.skype.com/macosx/Skype_6.2.0.1117.dmg',
+    provider => appdmg,
+  }
+
+  package { 'Tunnelblick':
+    source   => 'https://tunnelblick.googlecode.com/files/Tunnelblick_3.3beta21b.dmg',
+    provider => appdmg,
+  }
 }
