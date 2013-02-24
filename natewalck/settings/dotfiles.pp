bd813ac44717e113834a9d63aa681a7eea469562
natewalck <nate@osxadm.in>
2013-02-23 22:48:19 -0500

    Added params and .gitconfig

--- natewalck/settings/dotfiles.pp
+++ natewalck/settings/dotfiles.pp
@@ -1,6 +1,14 @@
-class people::natewalck::settings::dotfiles {
+class people::natewalck::settings::dotfiles (
+  $my_homedir   = $people::natewalck::params::my_homedir,
+  $my_sourcedir = $people::natewalck::params::my_sourcedir,
+  $my_username  = $people::natewalck::params::my_username
+  ) {
 
-  $my_homedir = "/Users/${::luser}"
+
+  # Dotfile Repo
+  repository { "${my_sourcedir}/dotfiles":
+    source => 'natewalck/dotfiles',
+  }
 
   exec { "Clone vim Settings":  
     command => "git clone --recursive https://github.com/natewalck/vim.git ${my_homedir}/.vim",
@@ -18,4 +26,12 @@ class people::natewalck::settings::dotfiles {
     ensure => link,
     target => "${my_homedir}/.vim/vimrc",
   }
+
+  file { "${my_homedir}/.gitconfig":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_sourcedir}/dotfiles/.gitconfig",
+    require => Repository["${my_sourcedir}/dotfiles"],
+    }
+
 }
